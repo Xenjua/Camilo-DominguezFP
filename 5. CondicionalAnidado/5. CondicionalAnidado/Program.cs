ba9be3cb7667e1ab6586112fdfb616d0328cd83a
Xenjua <juank.dominguez21@gmail.com>
2025-08-15 15:30:54 -0500

    Add files via upload

---
+++ 5. CondicionalAnidado/5. CondicionalAnidado/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _5._CondicionalAnidado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("Ingresar numero 1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 3:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+
+                if (num1 > num3)
+                {
+                    Console.WriteLine($"numero1: {num1}, es el mayor");
+
+                }
+                else
+                {
+                    Console.WriteLine($"numero3: {num3}, es el mayor");
+
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine($"numero2: {num2}, es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine($"numero3: {num3}, es el mayor");
+                }
+            }
+        }
+    }
+}
