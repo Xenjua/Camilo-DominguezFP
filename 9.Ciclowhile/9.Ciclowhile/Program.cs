df1a4a2d7481085a0f3f9a5077100d04647f3ecf
Xenjua <juank.dominguez21@gmail.com>
2025-08-29 15:29:50 -0500

    Add files via upload

---
+++ 9.Ciclowhile/9.Ciclowhile/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _9.Ciclowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"C:{contador} -A: {acumulador}");
+                contador++;
+
+
+
+
+            }
+            Console.WriteLine($"la suma de los cinco primeros numeros enteros es: {acumulador}");
+
+            
+
+        }
+    }
+}
