22730a04e968eddbb55b2d34f34467596af17d61
Xenjua <juank.dominguez21@gmail.com>
2025-09-03 14:59:43 -0500

    Add files via upload

---
+++ 13.While5/13.While5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _13.While5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int cantidadPares = 0;
+            int rango = 0;
+
+            Console.WriteLine("Ingrese el rango de numeros a evaluar:");
+            rango = int.Parse(Console.ReadLine());
+
+            while (contador <= rango)
+            {
+                if (contador % 2 == 0)
+                {
+                    cantidadPares++;
+
+                }
+                contador++;
+
+
+
+            }
+            Console.WriteLine($"La cantidad de numeros pares del 1 hasta {rango}, es: {cantidadPares}");
+
+
+        }
+    }
+}
