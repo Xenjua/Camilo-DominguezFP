ef7695ae39e30c0efb39c9fea6fdd10d1549863d
Xenjua <juank.dominguez21@gmail.com>
2025-10-17 14:27:17 -0500

    Add files via upload

---
+++ 29. Modular 1/29. Modular 1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _29._Modular_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplosDe5 = new int[CapturarFilas(), CapturarColumnas()];
+        }
+
+        static int CapturarFilas()
+        {
+            Console.WriteLine("Ingrese el número de filas para la matriz");
+            int filas = int.Parse(Console.ReadLine());
+            return filas;
+        }
+
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese el número de columnas para la matriz");
+            int columnas = int.Parse(Console.ReadLine());
+            return columnas;
+        }
+
+        static int[,] LlenarMatriz(int[,] matriz)
+        {
+            return matriz;
+        }
+
+
+    }
+}
+        
+    
+
