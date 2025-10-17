9eb6d48413096e5e111e0a9df6dfe4e0c5846bbb
Xenjua <juank.dominguez21@gmail.com>
2025-10-17 14:24:37 -0500

    Add files via upload

---
+++ 24. Ejercicio/24. Ejercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _24._Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //24.Algoritmo que permita solicitar 10 números, los cuales serán almacenados en un arreglo, al final, debe visualizar el promedio de esos números./
+
+            int[] numeros = new int[10];
+            int suma = 0;
+
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Digite el número {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+                suma += numeros[i];
+            }
+
+
+            double promedio = (double)suma / numeros.Length;
+
+            Console.WriteLine($"\nEl promedio de los números ingresados es: {promedio}");
+        }
+    }
+}
+        
+    
+
