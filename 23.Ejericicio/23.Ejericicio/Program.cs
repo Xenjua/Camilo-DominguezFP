6a68f862fd95835a6047f142f91a5663d35b8751
Xenjua <juank.dominguez21@gmail.com>
2025-10-17 14:23:43 -0500

    Add files via upload

---
+++ 23.Ejericicio/23.Ejericicio/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _23.Ejericicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*23. Crear arreglo llamado "numeros" de 100 elementos asignar el número 10 en cada una de las posiciones del arreglo. 
+             * Leer el contenido de cada elemento y escribirlo en pantalla.*/
+
+            int[] numeros = new int[100];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 10;
+            }
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Posición {i}: {numeros[i]}");
+            }
+        }
+    }
+}
+        
+    
+
